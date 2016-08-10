2c99d899cdbc25de64167238b64342ae44e76cf7
BOLLE Sebastien <sebastien.bolle@orange.com>
2016-08-10 10:33:21 +0200

    commons: generate CustomAttribute xml payload without jaxb.
    Signed-off-by: BOLLE Sebastien <sebastien.bolle@orange.com>
    Signed-off-by: BONNARDEL Gregory <gbonnardel.ext@orange.com>

--- org.eclipse.om2m.commons/src/main/java/org/eclipse/om2m/commons/resource/CustomAttributeAdapter.java
+++ org.eclipse.om2m.commons/src/main/java/org/eclipse/om2m/commons/resource/CustomAttributeAdapter.java
@@ -7,22 +7,16 @@
  *******************************************************************************/
 package org.eclipse.om2m.commons.resource;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.Marshaller;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
-import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 
 public class CustomAttributeAdapter extends XmlAdapter<Element, CustomAttribute> {
 
 	private DocumentBuilder documentBuilder;
-	private JAXBContext jaxbContext;
 
 	@Override
 	public CustomAttribute unmarshal(Element v) throws Exception {
@@ -39,29 +33,20 @@ public class CustomAttributeAdapter extends XmlAdapter<Element, CustomAttribute>
 	@Override
 	public Element marshal(CustomAttribute v) throws Exception {
 		
+		
 		if (null == v) {
 			return null;
 		}
 
-		// 1. Build the JAXBElement to wrap the instance of Parameter.
-		QName rootElement = new QName(v.getCustomAttributeName());
 		Object value = v.getCustomAttributeValue();
-		Class<?> type = null;
-		if (value != null) {
-			type = value.getClass();
-		} else {
-			type = String.class;
-		}
-		
-		JAXBElement jaxbElement = new JAXBElement(rootElement, type, value);
 
 		Element e = null;
 		try {
 			Document document = getDocumentBuilder().newDocument();
-		Marshaller marshaller = getJAXBContext(type).createMarshaller();
-		marshaller.marshal(jaxbElement, document);
-		/*Element*/ e = document.getDocumentElement();
-		e.setAttribute("type", v.getCustomAttributeType());
+			e = document.createElement(v.getCustomAttributeName());
+			e.setTextContent((value != null ? value.toString() : ""));
+			e.setAttribute("type", v.getCustomAttributeType());
+			
 		} catch (Throwable t) {
 			t.printStackTrace();
 		}
@@ -78,13 +63,5 @@ public class CustomAttributeAdapter extends XmlAdapter<Element, CustomAttribute>
 		return documentBuilder;
 	}
 
-	private JAXBContext getJAXBContext(Class<?> type) throws Exception {
-		if (null == jaxbContext) {
-			// A JAXBContext was not set, so create a new one based on the type.
-//			return JAXBContext.newInstance(type);
-			jaxbContext = JAXBContext.newInstance("org.eclipse.om2m.commons.resource", this.getClass().getClassLoader());
-		}
-		return jaxbContext;
-	}
 
 }
