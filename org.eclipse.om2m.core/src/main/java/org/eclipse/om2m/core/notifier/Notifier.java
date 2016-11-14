24b27ad9b1f71f6db093d116ebdfcc0a25aea930
Mahdi Ben Alaya <benalaya@sensinov.com>
2016-11-14 01:07:11 +0100

    Fix Bug 507443 - Use JSON format by default for notifications
    Signed-off-by: Mahdi Ben Alaya <benalaya@sensinov.com>

--- org.eclipse.om2m.core/src/main/java/org/eclipse/om2m/core/notifier/Notifier.java
+++ org.eclipse.om2m.core/src/main/java/org/eclipse/om2m/core/notifier/Notifier.java
@@ -109,12 +109,12 @@ public class Notifier {
 				notification.setSubscriptionReference(subscriptionEntity.getHierarchicalURI());
 				notification.setSubscriptionDeletion(false);
 				RequestPrimitive notifRequest = new RequestPrimitive();
-				notifRequest.setContent(DataMapperSelector.getDataMapperList().get(MimeMediaType.XML).objToString(notification));
+				notifRequest.setContent(DataMapperSelector.getDataMapperList().get(MimeMediaType.JSON).objToString(notification));
 				notifRequest.setFrom("/" + Constants.CSE_ID);
 				notifRequest.setTo(uri);
 				notifRequest.setOperation(Operation.NOTIFY);
-				notifRequest.setRequestContentType(MimeMediaType.XML);
-				notifRequest.setReturnContentType(MimeMediaType.XML);
+				notifRequest.setRequestContentType(MimeMediaType.JSON);
+				notifRequest.setReturnContentType(MimeMediaType.JSON);
 				ResponsePrimitive resp = notify(notifRequest, uri);
 				if(resp.getResponseStatusCode().equals(ResponseStatusCode.TARGET_NOT_REACHABLE)){
 					throw new Om2mException("Error during the verification request", 
@@ -254,15 +254,15 @@ public class Notifier {
 				if(sub.getNotificationContentType().equals(NotificationContentType.MODIFIED_ATTRIBUTES)){
 					serializableResource = (Resource)mapper.mapEntityToResource(resource, ResultContent.ATTRIBUTES);
 					notification.getNotificationEvent().setRepresentation(serializableResource);
-					request.setRequestContentType(MimeMediaType.XML);
+					request.setRequestContentType(MimeMediaType.JSON);
 				} else if(sub.getNotificationContentType().equals(NotificationContentType.WHOLE_RESOURCE)){
 					serializableResource = (Resource) mapper.mapEntityToResource(resource, ResultContent.ATTRIBUTES);
 					notification.getNotificationEvent().setRepresentation(serializableResource);
-					request.setRequestContentType(MimeMediaType.XML);
+					request.setRequestContentType(MimeMediaType.JSON);
 				} 
 			} 
 			// Set the content
-			request.setContent(DataMapperSelector.getDataMapperList().get(MimeMediaType.XML).objToString(notification));
+			request.setContent(DataMapperSelector.getDataMapperList().get(MimeMediaType.JSON).objToString(notification));
 			// For each notification URI: send the notify request
 			for(final String uri : sub.getNotificationURI()){
 				CoreExecutor.postThread(new Runnable(){
