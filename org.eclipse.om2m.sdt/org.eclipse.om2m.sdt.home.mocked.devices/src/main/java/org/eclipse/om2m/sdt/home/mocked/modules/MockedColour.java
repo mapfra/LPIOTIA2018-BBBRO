8e2b66224201a789bbea19cf9229867cda5df151
BOLLE Sebastien <sebastien.bolle@orange.com>
2017-08-23 16:57:51 +0200

    sdt.mocked.devices: use get/set grouped datapoint feature.
    Added a specialized data handler for grouped datapoints red/green/blue.
    
    Signed-off-by: BOLLE Sebastien <sebastien.bolle@orange.com>
    Signed-off-by: Cyrille Bareau <cyrille.bareau@orange.com>
    Signed-off-by: BONNARDEL Gregory <gbonnardel.ext@orange.com>

--- org.eclipse.om2m.sdt/org.eclipse.om2m.sdt.home.mocked.devices/src/main/java/org/eclipse/om2m/sdt/home/mocked/modules/MockedColour.java
+++ org.eclipse.om2m.sdt/org.eclipse.om2m.sdt.home.mocked.devices/src/main/java/org/eclipse/om2m/sdt/home/mocked/modules/MockedColour.java
@@ -7,50 +7,90 @@
  *******************************************************************************/
 package org.eclipse.om2m.sdt.home.mocked.modules;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.eclipse.om2m.sdt.Domain;
 import org.eclipse.om2m.sdt.datapoints.IntegerDataPoint;
+import org.eclipse.om2m.sdt.exceptions.AccessException;
 import org.eclipse.om2m.sdt.exceptions.DataPointException;
+import org.eclipse.om2m.sdt.home.mocked.devices.Activator;
 import org.eclipse.om2m.sdt.home.modules.Colour;
 import org.eclipse.om2m.sdt.home.types.DatapointType;
 
 public class MockedColour extends Colour {
+	
+	static private int red, green, blue;
 
 	public MockedColour(String name, Domain domain) {
 		super(name, domain,
 			new IntegerDataPoint(DatapointType.red) {
-				private Integer v = new Integer((int)(Math.random() * 255));
 				@Override
 				public void doSetValue(Integer value) throws DataPointException {
-					v = value;
+					Activator.logger.info("set single red " + value);
+					red = value;
 				}
 				@Override
 				public Integer doGetValue() throws DataPointException {
+					int v = (red == 0) ? (int)(Math.random() * 255) : red;
+					Activator.logger.info("get single red " + v);
 					return v;
 				}
 			}, 
 			new IntegerDataPoint(DatapointType.green) {
-				private Integer v = new Integer((int)(Math.random() * 255));
 				@Override
 				public void doSetValue(Integer value) throws DataPointException {
-					v = value;
+					green = value;
 				}
 				@Override
 				public Integer doGetValue() throws DataPointException {
-					return v;
+					return (green == 0) ? (int)(Math.random() * 255) : green;
 				}
 			}, 
 			new IntegerDataPoint(DatapointType.blue) {
-				private Integer v = new Integer((int)(Math.random() * 255));
 				@Override
 				public void doSetValue(Integer value) throws DataPointException {
-					v = value;
+					blue = value;
 				}
 				@Override
 				public Integer doGetValue() throws DataPointException {
-					return v;
+					return (blue == 0) ? (int)(Math.random() * 255) : blue;
 				}
 			}
 		);
+		setDatapointHandler(new DatapointHandler() {
+			@Override
+			public void setValues(Map<String, Object> values)
+					throws DataPointException, AccessException {
+				Activator.logger.info("set values " + values);
+				for (Map.Entry<String, Object> entry : values.entrySet()) {
+					switch (entry.getKey()) {
+					case "red": red = (int)entry.getValue(); break;
+					case "green": green = (int)entry.getValue(); break;
+					case "blue": blue = (int)entry.getValue(); break;
+					default:
+						break;
+					}
+				}
+			}
+			@Override
+			public Map<String, Object> getValues(List<String> names)
+					throws DataPointException, AccessException {
+				Map<String, Object> ret = new HashMap<String, Object>();
+				for (String name : names) {
+					switch (name) {
+					case "red": ret.put(name, red); break;
+					case "green": ret.put(name, green); break;
+					case "blue": ret.put(name, blue); break;
+					default:
+						break;
+					}
+				}
+				Activator.logger.info("get values " + names + " -> " + ret);
+				return ret;
+			}
+		});
 	}
 
 }
