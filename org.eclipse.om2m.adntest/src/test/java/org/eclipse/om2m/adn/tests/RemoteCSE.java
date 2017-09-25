53e3aef346a5c4cd0a6c4e29db44f284c148296a
Mahdi Ben Alaya <benalaya@sensinov.com>
2017-09-25 10:42:27 +0200

    add license header to adntest
    Signed-off-by: Mahdi Ben Alaya <benalaya@sensinov.com>

--- org.eclipse.om2m.adntest/src/test/java/org/eclipse/om2m/adn/tests/RemoteCSE.java
+++ org.eclipse.om2m.adntest/src/test/java/org/eclipse/om2m/adn/tests/RemoteCSE.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2016- 2017 SENSINOV (www.sensinov.com)
+ * 41 Rue de la découverte 31676 Labège - France 
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.eclipse.om2m.adn.tests;
 
 import static org.junit.Assert.*;
--- org.eclipse.om2m.adntest/src/test/java/org/eclipse/om2m/adn/tools/RestHttpClient.java
+++ org.eclipse.om2m.adntest/src/test/java/org/eclipse/om2m/adn/tools/RestHttpClient.java
@@ -1,27 +1,12 @@
-/*****************************************************************************************
-* SENSINOV ("COMPANY") CONFIDENTIAL
-* Copyright (c) 2016 SENSINOV, All Rights Reserved.
-*
-* NOTICE:  All information contained herein is, and remains the property of COMPANY. 
-* The intellectual and technical concepts contained herein are proprietary to COMPANY 
-* and may be covered by France and Foreign Patents, patents in process, and are protected 
-* by trade secret or copyright law. Dissemination of this information or reproduction 
-* of this material is strictly forbidden unless prior written permission is obtained
-* from COMPANY. Access to the source code contained herein is hereby forbidden to 
-* anyone except current COMPANY employees, managers or contractors who have executed 
-* Confidentiality and Non-disclosure agreements explicitly covering such access.
-*
-* The copyright notice above does not evidence any actual or intended publication or 
-* disclosure  of  this source code, which includes * information that is confidential 
-* and/or proprietary, and is a trade secret, of  COMPANY. ANY REPRODUCTION, MODIFICATION,
-* DISTRIBUTION, PUBLIC  PERFORMANCE, OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE 
-* CODE  WITHOUT  THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED, AND IN 
-* VIOLATION OF APPLICABLE * LAWS AND INTERNATIONAL TREATIES. THE RECEIPT OR POSSESSION OF
-* THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS TO 
-* REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL ANYTHING
-* THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.                
-*******************************************************************************************/
-
+/*******************************************************************************
+ * Copyright (c) 2016- 2017 SENSINOV (www.sensinov.com)
+ * 41 Rue de la découverte 31676 Labège - France 
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.eclipse.om2m.adn.tools;
 
 import java.io.File;
