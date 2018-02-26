202b92596119c6d0e470023493fd33cfe089e575
Mahdi Ben Alaya <benalaya@sensinov.com>
2018-02-26 13:49:04 +0100

    add oneM2M java clients

---
+++ org.eclipse.om2m.client.java/src/main/java/org/eclipse/om2m/client/java/tools/HttpResponse.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2016- 2017 SENSINOV (www.sensinov.com)
+ * 41 Rue de la découverte 31676 Labège - France 
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.om2m.client.java.tools;
+
+public class HttpResponse {
+	private int statusCode;
+	private String body;
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+	
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+	
+	public String getBody() {
+		return body;
+	}
+	
+	public void setBody(String body) {
+		this.body = body;
+	}
+}
